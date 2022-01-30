4267b3783cac41c4fe5aa44cbc8567f536d46a6c
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2022-01-30 19:30:24 -0300

    add: images dynamic and remover: mocks folder

--- miragejs/seeds/index.js
+++ miragejs/seeds/index.js
@@ -2,18 +2,16 @@
  * Mirage JS guide on Seeds: https://miragejs.com/docs/data-layer/factories#in-development
  */
 
-// eslint-disable-next-line prettier/prettier
-const usersSeeder = server => {
-  /*
-   * This will create in the in memory DB 10 objects
-   * of the Factory `user`. Moreover it creates a
-   * random number of messages and assign to each
-   * and every user, making use of relationships.
-   */
+const usersSeeder = (server) => {
   server.createList('user', 15);
+};
+
+const productSeeder = (server) => {
+  server.createList('product', 15);
 };
 
 export default function seeds(server) {
   server.loadFixtures();
   usersSeeder(server);
+  productSeeder(server);
 }
