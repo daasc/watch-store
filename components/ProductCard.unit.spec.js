15203994f5b681e8f923ab4d8768d2caea5ecde6
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2022-02-01 19:45:11 -0300

    add: add mountProductCart

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,5 +1,25 @@
+/* eslint-disable no-undef */
 import { mount } from '@vue/test-utils'
 import ProductCard from '@/components/ProductCard.vue'
+
+const mountProductCart = () => {
+  const product = server.create('product', {
+    title: 'hello',
+    price: '20.00',
+    image:
+      'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80',
+  })
+
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        products: product,
+      },
+    }),
+    product,
+  }
+}
+
 describe('ProductCard - Unit', () => {
   let server
   beforeEach(() => {
@@ -9,30 +29,24 @@ describe('ProductCard - Unit', () => {
     server.shutdown()
   })
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        products: server.create('product', {
-          title: 'hello',
-          price: '20.00',
-          image:
-            'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80',
-        }),
-      },
-    })
+    const { wrapper } = mountProductCart()
+
     expect(wrapper.element).toMatchSnapshot()
   })
 
   it('should mount the component ', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        products: server.create('product', {
-          title: 'hello',
-          price: '20.00',
-        }),
-      },
-    })
+    const { wrapper } = mountProductCart()
     expect(wrapper.vm).toBeDefined()
-    console.log(wrapper.html())
     expect(wrapper.text()).toContain('20.00')
+  })
+
+  // eslint-disable-next-line require-await
+  it('should emit the event addToCart with product object when button gets clicked ', async () => {
+    const { wrapper, product } = mountProductCart()
+    wrapper.find('button').trigger('click')
+
+    expect(wrapper.emitted().addToCart).toBeTruthy()
+    expect(wrapper.emitted().addToCart.length).toBe(1)
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
   })
 })
