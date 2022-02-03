35d625460ca6cf893b4ced7608dffa89c45290b7
Paulo Sobrinho Ferreira <paulo.sobrinho@opabox.com.br>
2022-02-03 20:27:24 -0300

    add: test integration search

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -47,7 +47,6 @@ describe('ProductList - Integration', () => {
     const card = wrapper.findAllComponents(ProductCard)
     expect(card).toHaveLength(10)
   })
-
   it('should display the error message when Promise rejects', async () => {
     axios.get.mockReturnValue(Promise.reject(new Error('')))
     const wrapper = mount(ProductList, {
@@ -59,5 +58,59 @@ describe('ProductList - Integration', () => {
     await Vue.nextTick()
 
     expect(wrapper.text()).toContain('Error the load carts')
+  })
+  it('should filter the product list when a search is performed', async () => {
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'i love watch',
+      }),
+      server.create('product', {
+        title: 'i hate watch',
+      }),
+    ]
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }))
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    })
+    await Vue.nextTick()
+
+    const search = wrapper.findComponent(Search)
+    await search.find('input[type="search"]').setValue('watch')
+    await search.find('form').trigger('submit')
+
+    const cards = wrapper.findAllComponents(ProductCard)
+    expect(wrapper.vm.searchTerm).toEqual('watch')
+    expect(cards).toHaveLength(2)
+  })
+
+  it('should clean filter and return all products', async () => {
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'i love watch',
+      }),
+    ]
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }))
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    })
+    await Vue.nextTick()
+
+    const search = wrapper.findComponent(Search)
+    await search.find('input[type="search"]').setValue('watch')
+    await search.find('form').trigger('submit')
+    await search.find('input[type="search"]').setValue('')
+    await search.find('form').trigger('submit')
+
+    const cards = wrapper.findAllComponents(ProductCard)
+    expect(wrapper.vm.searchTerm).toEqual('')
+    expect(cards).toHaveLength(11)
   })
 })
